9f793c572e32ade6e2a1d2658313e9af65e0e104
la .varik. .VALefor. <varikvalefor@aol.com>
2025-07-11 17:15:09 -0400

    ni'o ciksi le jai filri'a be lo me'oi .Markdown. versiio

---
+++ mkmd.sh
@@ -0,0 +1,5 @@
+mkdir markdown;
+ls -1F | grep '\/$' | xargs -Ifuck mkdir markdown/fuck;
+mkdir markdown/50x/toolbox;
+cat drawings.tex  | pcregrep -o '{.*?\.(png|jpg)}' | sed -e 's/^{//' | sed -e 's/}$//' | xargs -Ifuck ffmpeg -i "fuck" -vf scale=512:-1 "markdown/fuck-cmalu.webp";
+perl -pe 's/{(.*?(png|jpg))}/{\1-cmalu.webp}/' < drawings.tex | pandoc -f latex -i - -o markdown/drawings.md;
