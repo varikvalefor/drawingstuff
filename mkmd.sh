19b9f7730a0bfd9eb05cff203451901876338e4c
la .varik. .VALefor. <varikvalefor@aol.com>
2025-07-11 17:25:01 -0400

    ni'o cusku le se du'u zbasu fi ma kau

--- mkmd.sh
+++ mkmd.sh
@@ -2,4 +2,12 @@ mkdir markdown;
 ls -1F | grep '\/$' | xargs -Ifuck mkdir markdown/fuck;
 mkdir markdown/50x/toolbox;
 cat drawings.tex  | pcregrep -o '{.*?\.(png|jpg)}' | sed -e 's/^{//' | sed -e 's/}$//' | xargs -Ifuck ffmpeg -i "fuck" -vf scale=512:-1 "markdown/fuck-cmalu.webp";
-perl -pe 's/{(.*?(png|jpg))}/{\1-cmalu.webp}/' < drawings.tex | pandoc -f latex -i - -o markdown/index.md;
+
+if [ $(git branch --show-current) = "lojban" ];
+then
+	HEADER="# le te zbasu\nni'o le zmiku cu zbasu le me'oi .Markdown. versiio le me'oi .LaTeX. versiio be la'o zoi. +/ Drawings .zoi. noi .urli ke 'a fa zoi .urli. http://git.varikvalefor.i2p/vvx/drawings .urli.";
+else
+	HEADER="# The Source Material\nThe Markdown thing is automatically created from the LaTeX version (of \"+/ Drawings\"), which is available at <http://git.varikvalefor.i2p>.";
+fi
+
+(echo "$HEADER\n\n"; perl -pe 's/{(.*?(png|jpg))}/{\1-cmalu.webp}/' < drawings.tex | pandoc -f latex -i - -t markdown -o -) > markdown/index.md;
