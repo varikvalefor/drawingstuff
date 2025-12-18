bf3f2f1516f8928e3321ea9a2efa3756f32d2cb5
la .varik. .VALefor. <varikvalefor@aol.com>
2025-12-17 20:22:11 -0500

    ni'o vimcu pe'a le me'oi .semicolon.

--- mkmd.sh
+++ mkmd.sh
@@ -3,7 +3,7 @@ ls -1F | grep '\/$' | xargs -Ifuck mkdir markdown/fuck;
 mkdir markdown/50x/toolbox;
 for i in $(cat drawings.tex  | pcregrep -o '{.*?\.(png|jpg)}' | sed -e 's/^{//' | sed -e 's/}$//'); do ffmpeg -i "$i" -n -vf scale=512:-1 -compression_level 6 -quality 50 "markdown/$i-cmalu.webp"; done
 
-if [ $(git branch --show-current) = "lojban" ];
+if [ $(git branch --show-current) = "lojban" ]
 then
 	HEADER="# le te zbasu\nni'o le zmiku cu zbasu le me'oi .Markdown. versiio le me'oi .LaTeX. versiio be la'o zoi. +/ Drawings .zoi. noi .urli ke'a fa zoi .urli. http://git.varikvalefor.i2p/vvx/drawings .urli.";
 else
