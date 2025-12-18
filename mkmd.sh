13a53303df098b398c70a6e647665728e46d655d
la .varik. .VALefor. <varikvalefor@aol.com>
2025-12-17 20:22:02 -0500

    ni'o vimcu pe'a le me'oi .debug. co'e

--- mkmd.sh
+++ mkmd.sh
@@ -1,6 +1,5 @@
 mkdir markdown;
 ls -1F | grep '\/$' | xargs -Ifuck mkdir markdown/fuck; 
-ls -1F | grep '\/$';
 mkdir markdown/50x/toolbox;
 for i in $(cat drawings.tex  | pcregrep -o '{.*?\.(png|jpg)}' | sed -e 's/^{//' | sed -e 's/}$//'); do ffmpeg -i "$i" -n -vf scale=512:-1 -compression_level 6 -quality 50 "markdown/$i-cmalu.webp"; done
 
