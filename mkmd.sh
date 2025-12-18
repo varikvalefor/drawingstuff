fc4d9ae6ed46e1b18d42aca399d24f752dc4e492
la .varik. .VALefor. <varikvalefor@aol.com>
2025-12-17 20:26:48 -0500

    ni'o ningau pe'a

--- mkmd.sh
+++ mkmd.sh
@@ -1,11 +1,15 @@
 mkdir markdown;
-ls -1F | grep '\/$' | xargs -Ifuck mkdir markdown/fuck;
-mkdir markdown/50x/toolbox;
-cat drawings.tex  | pcregrep -o '{.*?\.(png|jpg)}' | sed -e 's/^{//' | sed -e 's/}$//' | xargs -Ifuck ffmpeg -i "fuck" -vf scale=512:-1 "markdown/fuck-cmalu.webp";
+ls -1F | grep '\/$' | xargs -Ifuck mkdir markdown/fuck; 
+mkdir -p markdown/50x/toolbox;
 
-if [ $(git branch --show-current) = "lojban" ];
+for i in $(cat drawings.tex  | pcregrep -o '{.*?\.(png|jpg)}' | sed -e 's/^{//' | sed -e 's/}$//')
+do
+	ffmpeg -i "$i" -n -vf scale=512:-1 -compression_level 6 -quality 50 "markdown/$i-cmalu.webp";
+done
+
+if [ $(git branch --show-current) = "lojban" ]
 then
-	HEADER="# le te zbasu\nni'o le zmiku cu zbasu le me'oi .Markdown. versiio le me'oi .LaTeX. versiio be la'o zoi. +/ Drawings .zoi. noi .urli ke 'a fa zoi .urli. http://git.varikvalefor.i2p/vvx/drawings .urli.";
+	HEADER="# le te zbasu\nni'o le zmiku cu zbasu le me'oi .Markdown. versiio le me'oi .LaTeX. versiio be la'o zoi. +/ Drawings .zoi. noi .urli ke'a fa zoi .urli. http://git.varikvalefor.i2p/vvx/drawings .urli.";
 else
 	HEADER="# The Source Material\nThe Markdown thing is automatically created from the LaTeX version (of \"+/ Drawings\"), which is available at <http://git.varikvalefor.i2p>.";
 fi
